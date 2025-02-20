9064d392b7ea1abe82d24a86d8fe3ae7ad1c9ff3
Hashir416-web <hashufazal@gmail.com>
2025-02-20 20:45:06 +1100

    Data Types

---
+++ Data Types.py
@@ -0,0 +1,14 @@
+a=5
+print ("type of a is: ",type(a))
+b=2.5
+print ("type of b is" ,type(b))
+c=True
+print ("type of c is" ,type(c))
+d="Sneha"
+print ("type of d is" ,type(d))
+E=int (b)
+print (type(E))
+print(E)
+text= str(input("Enter the word "))
+f=text[0:3]
+print(f)
