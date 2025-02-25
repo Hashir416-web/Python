1f010767cb2ec452363ec90683e690e9fda76d62
Hashir416-web <hashufazal@gmail.com>
2025-02-25 20:43:11 +1100

    operators

---
+++ oprtor.py
@@ -0,0 +1,8 @@
+a = 91
+b = 23
+c = 90
+d = 11
+e = 88
+sum = a+b+c+d+e
+average = sum/5
+print ("average of the height of all the tree's" ,average)
---
+++ percetage.py
@@ -0,0 +1,10 @@
+print ("Enter the average percentage of the marks")
+math =int(input("maths :"))
+english = int(input("english :"))
+science = int(input("science :"))
+hindi = int(input("hindi :"))
+sum = math+english+science+hindi
+print (sum )
+perc= (sum/400)*100
+print ("percentage mark =")
+print (perc)
